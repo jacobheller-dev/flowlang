d43fecbe00259361b34b816ee8b72880caf9f982
Jacob Heller <109388149+jacobheller-dev@users.noreply.github.com>
2023-06-05 11:01:25 -0700

    Create main.py

---
+++ FLOW/main.py
@@ -0,0 +1,5 @@
+# commit main.py instruction Jacob Heller (c) 2023
+
+# copy flow.py to target folder
+# execute flow.py with index.flo accessible in target folder with unmodified hierarchy
+# script self-executes in open terminal
